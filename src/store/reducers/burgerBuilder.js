5722108fdd37fe376edaaf449349b064c391d16f
Usuario <stmuraro@gmail.com>
2020-08-14 10:51:32 -0300

    section 17 completed

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes'
+import {updateObject} from '../utility'
 
 const initialState = {
   ingredients: null, // fetching from Firebase
@@ -13,6 +14,68 @@ const INGREDIENT_PRICES = {
   bacon: 0.7
 }
 
+const addIngredient = (state, action) => {
+  const updatedIngredient1 = {[action.ingName]: state.ingredients[action.ingName] + 1}
+  const updatedIngredients1 = updateObject(state.ingredients, updatedIngredient1)
+  const updatedState1 = {
+    ingredients: updatedIngredients1,
+    totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingName]
+  }
+  return updateObject(state, updatedState1)
+} 
+
+const removeIngredient = (state, action) => {
+  const updatedIngredient2 = {[action.ingName]: state.ingredients[action.ingName] - 1}
+  const updatedIngredients2 = updateObject(state.ingredients, updatedIngredient2)
+  const updatedState2 = {
+    ingredients: updatedIngredients2,
+    totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingName]
+  }
+  return updateObject(state, updatedState2)
+}
+
+const clearIngredient = (state, action) => {
+  const updatedIngredients3 = {
+    salad: 0,
+    bacon: 0,
+    cheese: 0,
+    meat: 0
+  }
+  const updatedState3 = {
+    ingredients: updatedIngredients3,
+    totalPrice: 4
+  }
+  return updateObject(state, updatedState3)
+}
+
+const setIngredients = (state, action) => {
+  const updatedState4 = {
+    ingredients: action.ingredients,
+    error: false
+  }
+  return updateObject(state, updatedState4)
+}
+
+const fetchIngredientsFailed = (state, action) => {
+  const updatedState5 = {error: true}
+  return updateObject(state, updatedState5)
+}
+
+const burgerBuilderReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.ADD_INGREDIENT: return addIngredient(state, action)
+    case actionTypes.REMOVE_INGREDIENT: return removeIngredient(state, action)
+    case actionTypes.CLEAR_INGREDIENTS: return clearIngredient(state, action)
+    case actionTypes.SET_INGREDIENTS: return setIngredients(state, action)
+    case actionTypes.FETCH_INGREDIENTS_FAILED: return fetchIngredientsFailed(state, action)
+    default: return state
+  }
+}
+
+export default burgerBuilderReducer
+
+
+/* OLD APPROACH
 const burgerBuilderReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
@@ -58,6 +121,4 @@ const burgerBuilderReducer = (state = initialState, action) => {
       }
     default: return state
   }
-}
-
-export default burgerBuilderReducer
+}*/
