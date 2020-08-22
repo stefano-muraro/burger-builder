2da022ff4d4b1774a34970ed7f1593667854fadf
Usuario <stmuraro@gmail.com>
2020-08-22 16:01:37 -0300

    add routes guard

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Layout from "./containers/Layout/Layout"
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
 import Checkout from "./containers/Checkout/Checkout"
-import {Route, Switch} from "react-router-dom"
+import {Route, Switch, Redirect} from "react-router-dom"
 import Orders from "./containers/Orders/Orders"
 import Auth from './containers/Auth/Auth'
 import Logout from './containers/Auth/Logout/Logout'
+import {connect} from 'react-redux'
+import * as actions from './store/actions/index'
 
-function App() {
-  return (
-    <div>
-      <Layout>
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup() // we dispatch it from App component (the root component) because it doesn't matter which route we visit, this component is always loaded when the applications loads, hence, this actions is always dispatched.
+  }
+
+  render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth}/>
+        <Route path="/" exact component={BurgerBuilder}/>
+        <Redirect to='/'/> // for unknown routes
+      </Switch>
+    )
+  
+    if (this.props.isAuthenticated) {
+      routes = (
         <Switch>
           <Route path="/checkout" component={Checkout}/>
           <Route path="/orders" component={Orders}/>
-          <Route path="/auth" component={Auth}/>
           <Route path="/logout" component={Logout}/>
           <Route path="/" exact component={BurgerBuilder}/>
+          <Redirect to='/'/> // for unknown routes
         </Switch>
-      </Layout>
-    </div>
-  );
+      )
+    }
+    return (
+      <div>
+        <Layout>
+          {routes}
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+  return {
+    onTryAutoSignup: () => {dispatch(actions.authCheckState())}
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,12 +23,15 @@ export const authFail = (error) => {
 }
 
 export const logout = () => {
+  localStorage.removeItem('token')
+  localStorage.removeItem('expirationDate')
+  localStorage.removeItem('userId')
   return {
     type: actionTypes.AUTH_LOGOUT
   }
 }
 
-export const checkAuthTimeout = expirationTime => {
+export const checkAuthTimeout = (expirationTime) => {
   return dispatch => {
     setTimeout(() => {
       dispatch(logout())
@@ -51,6 +54,10 @@ export const auth = (email, password, isSignup) => {
     axios.post(url, authData)
       .then(response => {
         console.log(response);
+        const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000)
+        localStorage.setItem('token', response.data.idToken)
+        localStorage.setItem('userId', response.data.localId)
+        localStorage.setItem('expirationDate', expirationDate)
         dispatch(authSuccess(response.data.idToken, response.data.localId))
         dispatch(checkAuthTimeout(response.data.expiresIn))
       })
@@ -65,5 +72,23 @@ export const setAuthRedirectPath = (path) => {
   return {
     type: actionTypes.SET_AUTH_REDIRECT_PATH,
     path: path
+  }
+}
+
+export const authCheckState = () => {
+  return (dispatch) => {
+    const token = localStorage.getItem('token')
+    if(!token) {
+      dispatch(logout())
+    } else {
+      const expirationDate = new Date(localStorage.getItem('expirationDate')) // what we get from the local storage is a string
+      if(expirationDate > new Date()) {
+        const userId = localStorage.getItem('userId')
+        dispatch(authSuccess(token, userId))
+        dispatch(checkAuthTimeout((expirationDate.getTime() - new Date().getTime())/1000))
+      } else {
+        dispatch(logout())
+      }
+    }
   }
 }
